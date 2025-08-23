be8b2fe9b2c6d3bbff1811dfb00a37047f3f21f9
nikk-dev05 <nikhil98627@gmail.com>
2025-08-23 18:49:55 +0530

    Added a docker file

--- src/main/java/in/sp/main/services/Ai.java
+++ src/main/java/in/sp/main/services/Ai.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
+
 
 import in.sp.main.Entity.ChatRequest;
 import in.sp.main.Entity.ChatResponse;
